56f8b26e7e0f8c8c355ef075a666acdde55d0167
EandT <mishravedant285@gmail.com>
2025-09-10 02:46:04 +0530

    Problems

--- Problem Solving/Check_Power_of_2.c
+++ Problem Solving/Check_Power_of_2.c
@@ -1,9 +1,4 @@
-//  Problem 2 – Check if Number is Power of 2
-
-//  Problem: Write a function to check if a number is a power of 2.
-//  Why: Common in memory alignment, buffer sizes.
 #include<stdio.h>
-#include<stdint.h>
 #include<stdint.h>
 int main() {
     uint8_t count = 0;
