11db2d4dc7c47b440d0fce3c0e815f1d04de8ca5
EandT <mishravedant285@gmail.com>
2025-09-24 21:04:20 +0530

    Array and Vector

---
+++ Basic2.0/Array_Tr.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    int arr[10] = {1,2,4,6,5};
+    printf("%d",arr[10]);
+
+    
+}
---
+++ Basic2.0/Vector.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> v;
+    v = {1,2,3,4,5};
+    for(int x : v) {
+        cout<<" "<<x;
+    }
+}
