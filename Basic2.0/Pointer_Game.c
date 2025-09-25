0a74441b9c677addb1092bef0fe5dd9feae3754e
EandT <mishravedant285@gmail.com>
2025-09-25 05:42:15 +0530

    Pointer

--- Basic2.0/Pointer_Game.c
+++ Basic2.0/Pointer_Game.c
@@ -2,13 +2,20 @@
 
 long long int f = 0xffff123456771221;
 int main() {
-    char *paddress1 = (char*)(&f);
+
+    //Very good observation about Pointer
+    
+    short int *paddress1 = (short int*)(&f);
     printf("The value at address %p is %x\n",paddress1,*paddress1);
 
     paddress1 = paddress1 + 1;
     printf("The value at address %p is %x\n",paddress1,*paddress1);
 
-    paddress1 = paddress1 + 4;
+    int *paddress1 = (short int*)(&f);
     printf("The value at address %p is %x\n",paddress1,*paddress1);
+
+    paddress1 = paddress1 + 1;
+    printf("The value at address %p is %x\n",paddress1,*paddress1);
+
 
 }
